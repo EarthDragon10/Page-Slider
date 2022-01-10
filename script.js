65bd847af9b953a92e8085b9f3ad98bbd7a31ef1
EarthDragon10 <lagudadev@gmail.com>
2022-01-10 09:07:52 +0100

    feat: added logic of swipe pagination

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const pagination = document.querySelector("#pagination");
-// console.log(pagination);
-let pageCurrent = 1;
+const labelsGroup = document.querySelectorAll("#pagination > label");
+const pages = document.querySelector("#pages");
+const radioBtn = document.querySelectorAll("#pages > input[type=radio]");
+console.log(radioBtn);
+
+let touchedStartX = 0;
+let touchedEndX = 0;
+
+let pageCurrent = 0;
 
 pagination.addEventListener("click", (e) => {
   const item = e.target;
@@ -14,10 +21,46 @@ pagination.addEventListener("click", (e) => {
 });
 
 function setCheckedLabel(selectedPage) {
-  const classLabels = document.querySelectorAll("#pagination > label");
   console.log(classLabels);
 
-  classLabels[pageCurrent - 1].classList.remove("lab-checkmark");
-  classLabels[selectedPage - 1].classList.add("lab-checkmark");
+  labelsGroup[pageCurrent - 1].classList.remove("lab-checkmark");
+  labelsGroup[selectedPage - 1].classList.add("lab-checkmark");
+
   pageCurrent = selectedPage;
+}
+
+pages.addEventListener("touchstart", touchStart, false);
+pages.addEventListener("touchend", touchEnd, false);
+
+function touchStart(e) {
+  touchedStartX = e.changedTouches[0].screenX;
+}
+
+function touchEnd(e) {
+  touchedEndX = e.changedTouches[0].screenX;
+
+  touchedEndX < touchedStartX ? moveTo("next") : moveTo("prev");
+
+  console.log({ touchedStartX: touchedStartX, touchedEndX: touchedEndX });
+}
+
+function moveTo(direction) {
+  if (direction === "next" && pageCurrent < labelsGroup.length) {
+    labelsGroup[pageCurrent - 1].classList.remove("lab-checkmark");
+    labelsGroup[pageCurrent].classList.add("lab-checkmark");
+    radioBtn[pageCurrent].checked = true;
+    pageCurrent++;
+  } else if (direction === "prev" && pageCurrent > 1) {
+    labelsGroup[pageCurrent - 1].classList.remove("lab-checkmark");
+    labelsGroup[pageCurrent - 2].classList.add("lab-checkmark");
+    radioBtn[pageCurrent - 2].checked = true;
+    pageCurrent--;
+  }
+}
+
+function goToPage(numOfPage) {
+  labelsGroup[pageCurrent].classList.remove("lab-checkmark");
+  labelsGroup[numOfPage].classList.add("lab-checkmark");
+  radioBtn[numOfPage].checked = true;
+  pageCurrent = numOfPage;
 }
